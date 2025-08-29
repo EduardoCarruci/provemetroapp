c352185c7e2e9b2b6a2052be9ef8ccaf136ecb6e
EduardoCarruci <edugocarruci@gmail.com>
2025-08-28 23:24:49 -0400

    ✓ ActividadColectivaRepository fetchActividades returns a list of ActividadColectiva

--- test/app/data/repository/actividad_colectiva_repository_test.dart
+++ test/app/data/repository/actividad_colectiva_repository_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provemetroapp/app/data/model/actividad_colectiva.dart';
 import 'package:provemetroapp/app/data/repository/actividad_colectiva_repository.dart';
-
+//
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
